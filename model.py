455184bc72cf7517d83b3e40aab334171bd98f40
Christovito Hidajat <54348204+christovito@users.noreply.github.com>
2022-12-04 22:13:51 +0700

    Update model.py

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ dataset_val = pd.read_csv("data/SPARCS-2015-VAL.csv")
 x = dataset.iloc[:, :11]
 y = dataset.iloc[:, -1]
 
-x_val = dataset_val.iloc[:, :12]
+x_val = dataset_val.iloc[:, :11]
 y_val = dataset_val.iloc[:, :-1]
 
 xgb = XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1, colsample_bynode=1, colsample_bytree=1, gamma=0, learning_rate=0.1, max_delta_step=0, max_depth=12, min_child_weight=1, n_estimators=100, n_jobs=1, nthread=None, objective='binary:logistic', random_state=0, reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=None, silent=None, subsample=1, verbosity=1)
